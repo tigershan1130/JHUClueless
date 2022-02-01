f3c2da12638eb4814e98ab3852e98c361f467616
tigershan1130 <swq_shan10@hotmail.com>
2022-02-01 23:26:31 +0800

    Lobby System for online session

---
+++ LobbyServer/OnlineSubsystemPythonServer.py
@@ -0,0 +1,142 @@
+
+# Copyright (c) 2019 Ryan Post
+# This software is provided 'as-is', without any express or implied warranty. In no event will the authors be held liable for any damages arising from the use of this software.
+# Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+# 1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
+
+
+import cherrypy
+from cherrypy.lib import auth_digest
+from cherrypy import response
+import json
+import jsonpickle
+import time
+from time import sleep
+from threading import Thread
+import requests
+
+class Server(object):
+     def __init__(self):
+        self.registeredby = ''
+        self.name = ''
+        self.port = 0
+        self.map = ''
+        self.playercount = 0
+        self.maxplayers = 0
+        self.pwprotected = False
+        self.gamemode = ''
+        self.timeoflastheartbeat = 0
+		
+     def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.name== other.name and self.port == other.port
+        else:
+            return False
+
+class MasterServer(object):
+
+    def __init__(self):
+        # Time between heartbeat in seconds, this is passed to the client and kept in sync.
+        self.time_between_heartbeats = 30
+        self.serverlist = []
+        thread = Thread(target = self.heartbeat)
+        thread.start()
+
+
+    def heartbeat(self):
+        while True:
+            for server in self.serverlist:
+                delta = int(time.time()) - server.timeoflastheartbeat
+                if (delta > self.time_between_heartbeats):
+                    self.serverlist.remove(server)
+            sleep(1)
+            
+    @cherrypy.expose
+    def register_server(self, name, port, map, maxplayers, pwprotected, gamemode):
+        if self.server_exists(cherrypy.request.remote.ip, port):
+            self.internal_update_server(cherrypy.request.remote.ip, port, name, map, 0, maxplayers, pwprotected, gamemode)
+            return json.dumps({'error' : False, 'message' : 'Sucessfully updated your server [%s %s:%s] on the server browser.' % (name, cherrypy.request.remote.ip, port)})
+        else:
+            server = Server()
+            server.ip = cherrypy.request.remote.ip
+            server.name = name
+            server.port = port
+            server.map = map
+            server.maxplayers = maxplayers
+            server.pwprotected = pwprotected
+            server.gamemode = gamemode
+            server.timeoflastheartbeat = int(time.time())
+            try:
+                r = requests.get('http://' + cherrypy.request.remote.ip + ':' + port, timeout=2)
+            except requests.exceptions.ConnectTimeout:
+                # ports are not forwarded...
+                if (cherrypy.request.remote.ip != "127.0.0.1"):
+                    return json.dumps({'error' : True, 'message' : 'Unable to connect to server [%s %s:%s]. Please verify your ports are forwarded and your firewall is not blocking the game. Your server will not be visible in the Server Browser.' % (server.name, server.ip, server.port)})
+            except requests.exceptions.ConnectionError:
+                pass
+
+            self.serverlist.append(server)
+            return json.dumps({'error' : False, 'message' : 'Sucessfully added your server [%s %s:%s] to the server browser.' % (name, cherrypy.request.remote.ip, port), 'heartbeat' : self.time_between_heartbeats })
+
+
+    def internal_update_server(self, ip, port, name, map, playercount, maxplayers, pwprotected, gamemode):
+        for server in self.serverlist:
+            if server.ip == ip and server.port == port: 
+                server.name = name
+                server.map = map
+                server.playercount = playercount
+                server.maxplayers = maxplayers
+                server.pwprotected = pwprotected
+                server.gamemode = gamemode
+                server.timeoflastheartbeat = int(time.time())
+                return True
+        return False
+
+    @cherrypy.expose
+    def update_server(self, port, name, map, playercount, maxplayers, pwprotected, gamemode):
+        if self.internal_update_server(cherrypy.request.remote.ip, port, name, map, playercount, maxplayers, pwprotected, gamemode):
+            return json.dumps({'error' : False, 'message' : 'Sucessfully updated your server [%s %s:%s] on the server browser.' % (name, cherrypy.request.remote.ip, port)})
+        return json.dumps({'error' : True, 'message' : 'Server not registered'})
+         
+
+    def server_exists(self, ip, port):
+        for server in self.serverlist:
+            if server.ip == ip and server.port == port:
+                return True
+        return False
+
+    @cherrypy.expose
+    def unregister_server(self, port):
+        for server in self.serverlist:
+            if (server.registeredby == cherrypy.request.remote.ip and server.port == port):
+                self.serverlist.remove(server)
+
+    @cherrypy.expose
+    def get_serverlist(self):
+        self.returnlist = []
+        for server in self.serverlist:
+            jsonstring = {'name' : server.name, 'port' : server.port, 'map' : server.map, 'playercount' : server.playercount, 'maxplayers' : server.maxplayers, 'pwprotected' : server.pwprotected, 'gamemode' : server.gamemode, 'ip' : server.ip }
+            self.returnlist.append(jsonstring)
+        return json.dumps({'error' : False, 'message' : '', 'servers' : self.returnlist})
+
+    @cherrypy.expose
+    def perform_heartbeat(self, port):
+        for server in self.serverlist:
+            if (server.ip == cherrypy.request.remote.ip and server.port == port):
+                server.timeoflastheartbeat = int(time.time())
+
+
+
+
+
+cherrypy.config.update({ 'server.socket_port': 8081,
+                         'server.socket_host': '127.0.0.1',
+                         "server.ssl_module": "pyopenssl",
+                         'server.thread_pool' : 100
+                       })
+
+masterserver = MasterServer()
+cherrypy.quickstart(masterserver)
---
+++ LobbyServer/README.md
@@ -0,0 +1,96 @@
+# Online Subsystem Python
+Created by RyroNZ
+
+Online Subsystem Python is a self-hosted online subsystem for Creating, Finding and Joining Sessions over the internet using the standard session nodes. 
+It is based off the Null subsystem.
+
+![](https://puu.sh/El2ZS/e3abeb7e11.png)
+
+Example Video: https://puu.sh/EkmSl/d85c851ec2.mp4
+
+## Installing the Plugin
+
+Copy the Online Subsystem Python folder containing the .uplugin into the Plugins folder in your Project Directory.
+If the Plugins folder does not exist you can create it.
+Open your .uproject and add the following add “OnlineSubsystemPython” to the plugins list and set it to Enabled.
+```
+{
+    "FileVersion": 3,
+    "EngineAssociation": "4.22",
+    "Category": "",
+    "Description": "",
+    "Modules": [
+        {
+            "Name": "MyProjectName",
+            "Type": "Runtime",
+            "LoadingPhase": "Default",
+            "AdditionalDependencies": [
+                "FunctionalTesting"
+            ]
+        }
+    ],
+    "Plugins": [
+        {
+            "Name": "OnlineSubsystemPython",
+            "Enabled": true
+        }
+    ]
+}
+```
+In your DefaultEngine.ini under [OnlineSubsystem] change the DefaultPlatformService so it reads
+‘DefaultPlatformService=Python’
+```
+[OnlineSubsystem]
+DefaultPlatformService=Python
+bUseBuildIdOverride=true
+BuildIdOverride=262494
+PollingIntervalInMs=20
+bHasVoiceEnabled=true
+```
+
+## Running the Server
+
+Find the Server folder in the Plugin directory, copy that to the machine you want to run the server on
+(We use Digital Ocean to host our servers, YMMV)
+
+
+Install Python 3
+```
+$ python -m pip install --upgrade -r requirements.txt
+$ python3 OnlineSubsystemPythonServer.py
+```
+Your server should be up and running, if you wish to configure the port the server is running on you’ll find it
+specified in cherrypy.config.update() on line 125.
+```
+cherrypy.config.update({ 'server.socket_port': 8081,
+                         'server.socket_host': '0.0.0.0',
+                         "server.ssl_module": "pyopenssl",
+                         'server.thread_pool' : 100
+                        })
+```
+
+## Configuring Client
+
+
+Open OnlineSubsystemPythonConfig.cpp in the Source/Private directory
+
+Change the SeverAddress to the IP/Domain you are using for your server.
+```
+UOnlineSubsystemPythonConfig::UOnlineSubsystemPythonConfig()
+    : ServerAddress("127.0.0.1:8081")
+    , AuthorizationTicket("")
+{}
+```
+
+Please note, the plugin only has support for the following session settings key/value pairs.
+```
+SERVERNAME
+MAPNAME
+GAMEMODE
+PASSWORDPROTECTED
+PLAYERCOUNT
+```
+
+Everything should be working now and you should be able to host and join using the standard session nodes!
+
+If there are things not working, please email me at ryan@somethinglogical.co.nz
